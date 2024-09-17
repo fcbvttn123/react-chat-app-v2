411ca4a52bf4a36d013ba890ff277631c0d88be6
ngan <vutran@sheridancollege.ca>
2024-09-17 17:28:56 -0400

    Create controllers and routes folders

---
+++ backend/controllers/auth.js
@@ -0,0 +1,7 @@
+async function postLogin(req, res) {
+  res.json({ message: "Login" })
+}
+
+module.exports = {
+  postLogin,
+}
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,13 @@
 require("dotenv").config()
 const express = require("express")
+const authRoutes = require("./routes/auth")
 
 const app = express()
 
 // Middleware
 app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.json({ test: "Hello World" })
-})
+app.use("/api/auth", authRoutes)
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening for request on port ${process.env.PORT}`)
