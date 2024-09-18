a7853d3ef8bcd2e8581d934d8fec779b429e82f3
ngan <vutran@sheridancollege.ca>
2024-09-18 16:52:33 -0400

    Firebase Setup

---
+++ frontend/config/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWIlwqq7Pt8_UWTa1zZ7vQE6_MGYshMcA",
+  authDomain: "react-projects-fe292.firebaseapp.com",
+  projectId: "react-projects-fe292",
+  storageBucket: "react-projects-fe292.appspot.com",
+  messagingSenderId: "1000517600283",
+  appId: "1:1000517600283:web:48b03824333700b66a64be",
+}
+
+const app = initializeApp(firebaseConfig)
