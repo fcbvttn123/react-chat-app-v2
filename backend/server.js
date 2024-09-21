21474c598df7c2949c3a3f2263275b870a6194ad
ngan <vutran@sheridancollege.ca>
2024-09-20 20:41:17 -0400

    CORS Setup for backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 require("dotenv").config()
 const express = require("express")
 const authRoutes = require("./routes/auth")
+const cors = require("cors")
 
 const app = express()
 
 // Middleware
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+)
 app.use(express.json())
 
 app.use("/api/auth", authRoutes)
