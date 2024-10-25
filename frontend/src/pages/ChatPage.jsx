d8d15de35a87b765e4f0f3a8cca9c3fae7b9c77c
ngan <vutran@sheridancollege.ca>
2024-10-24 22:40:51 -0400

    Chat code is moved from App.jsx to ChatPage.jsx

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,7 +1,60 @@
+import { useState } from "react"
+import { ChatComponent } from "../chat/ChatComponent"
+import { SearchBar } from "../components/SearchBar"
+import { useAuthContext } from "../hooks/useAuthContext"
+import { AUTH_ACTION } from "../context/actions"
+import { useNavigate } from "react-router-dom"
+
 export function ChatPage() {
+  const [searchResult, setSearchResult] = useState(null)
+  const navigate = useNavigate()
+  const { userId, username, token, dispatch } = useAuthContext()
+  if (searchResult) {
+    // Check if username is in the db
+    let usernameExists = docExists(
+      import.meta.env.VITE_USER_TABLE,
+      searchResult
+    ).then((res) => res)
+    // If username is in the db, connect user to Stream and create/get chat channel
+    usernameExists &&
+      connectUserToStream(
+        token,
+        {
+          id: username,
+        },
+        streamChatClient
+      ).then(
+        (res) =>
+          res && createDirectChannel(username, searchResult, streamChatClient)
+      )
+    // Reset Search Bar
+    setSearchResult(null)
+  }
+  function handleLogout() {
+    dispatch({ type: AUTH_ACTION.LOGOUT })
+    navigate("/login")
+  }
   return (
     <div>
-      <h1>Chat Page</h1>
+      {username && (
+        <div style={{ marginBottom: 20 }}>
+          <p>Current username: {username}</p>
+          <button onClick={handleLogout}>Logout</button>
+          <SearchBar
+            placeholder="Username..."
+            setSearchResult={setSearchResult}
+          />
+        </div>
+      )}
+      {token && (
+        <ChatComponent
+          clientData={{
+            apiKey: import.meta.env.VITE_STREAM_API_KEY,
+            tokenOrProvider: token,
+            userData: { id: userId },
+          }}
+        />
+      )}
     </div>
   )
 }
